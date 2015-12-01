a6a194b2199e0d0a44c5dfbeab8683f5a35f13a7
anibha88 <anibha88@gmail.com>
2015-12-01 23:13:03 +0530

    Added twitter login

---
+++ ficks/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, '3XQDMyHpVqpnDx44QLzOvPwql', 'b4cCJDnGjTYA2jgwzOOHre1vE7SAHnu7c3QChyJEu4yu9jArVj'
+end
---
+++ ficks/db/migrate/20151201123647_add_token_to_users.rb
@@ -0,0 +1,7 @@
+class AddTokenToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :token, :string
+    add_column :users, :secret, :string
+    add_column :users, :profile_image, :string
+  end
+end
